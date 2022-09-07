d39bf4779beb4439c60cd175f58bb119c6e5b5bb
Lukas <prenner.products@gmail.com>
2022-09-07 18:47:04 +0200

    finito eqd

--- Exercise_Sheet_2/main.c
+++ Exercise_Sheet_2/main.c
@@ -6,6 +6,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <sys/wait.h>
 
 
 int exitvalue = 0;
@@ -33,13 +34,14 @@ int main(int argc, char* argv[]) {
         }
 
         // userinput mit strtok pharsen
-        char* inputstring [256];
+        char* inputstring [256] = {'\0'};
         char delimiter[] = " ";
         char* argument;
         int count = 1;
 
         argument = strtok(buff, delimiter);
         inputstring[0]= argument;
+        printf("mein Inputstring[0:%s\n", inputstring[0]);
 
         while(argument != NULL && count < 255){
             inputstring[count]= argument;
@@ -62,39 +64,54 @@ int main(int argc, char* argv[]) {
         fgets(firstLine, 254, myFileHandle);
         int firstLineCount = strlen(firstLine);
 
-        if(firstLineCount >= 4 && firstLine[0] == '#' && firstLine[2]== '!'){
+        if(firstLineCount >= 4 && firstLine[0] == '#' && firstLine[1]== '!'){
             isSkript = 1;
              // Zeilenumbruch am ende entfernen das Posix das nicht mag!
              firstLine[firstLineCount-1] = '\0';
-            /// DA FEHLT NO INTERPRETER
+
+
+            for ( char* interpreter = firstLine +2; *interpreter < firstLineCount; interpreter++ ) {
+                inputstring[0] = interpreter;
+                printf("Der Interpreter ist: %s", interpreter);
+
+            }
         }
         fclose(myFileHandle);
+       // inputstring[-1]= '\0';
 
 
         // Prozess erzeugen mit fork()
 
-        pid_t cpid = fork();
+        pid_t pid = fork();
 
-        if(cpid < 0){
+        if(pid < 0){
             perror("Error! Konnte nicht Forken!");
             continue;
         }
-        else if(cpid == 0){
+        else if(pid == 0){
             printf("Das ist  der Kindprozess!\n");
+            fflush(stdout);
+            if(isSkript == 1){
+                printf("das ist ein Skript btw :D\n");
+                printf("Inputsting[0] = Interpreter = %s", inputstring[1]);
+                execv(inputstring[0], inputstring);
+                sleep(10);
+            } else if(isSkript == 0){
+                printf("das ist Kein Skript \n");
+                printf("das inputstring+1 %s  \n", *inputstring+1);
+                execv(inputstring[1], inputstring+1);
+                sleep(10);
+            }
+            perror("Fehler bei execv()");
+            exit(-42);
+
         }
-        else if (cpid  > 0){
+
+        else if (pid  > 0){
             printf("Das ist der Elternprozess \n");
+            waitpid(pid, NULL, 0);
+            printf("Der Kindprozess hat sich beendet!");
         }
-
-
-
-
-
-
-
-
-
-
 
 
 
